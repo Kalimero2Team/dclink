564504d761f2238c0ce0a3dd5c2cabd8f1ce1316
byquanton <32410361+byquanton@users.noreply.github.com>
2022-07-05 21:02:12 +0200

    1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.kalimero2.team"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.1-SNAPSHOT"
 description = "Discord Bot for linking Minecraft Accounts with Discord Accounts"
 
 java {
