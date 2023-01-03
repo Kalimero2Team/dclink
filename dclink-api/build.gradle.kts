cc86e4805cb7093b570bfc3f19e232ad12cc6d09
byquanton <32410361+byquanton@users.noreply.github.com>
2023-01-04 00:48:51 +0100

    Add Publishing

--- dclink-api/build.gradle.kts
+++ dclink-api/build.gradle.kts
@@ -4,6 +4,20 @@ plugins {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "byquantonRepo"
+            url = if(version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://repo.byquanton.eu/snapshots")
+            } else {
+                uri("https://repo.byquanton.eu/releases")
+            }
+            credentials {
+                username = System.getenv("REPO_USERNAME")
+                password = System.getenv("REPO_PASSWORD")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
